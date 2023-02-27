1450374bc6330db19327136ef95252020201ca34
Ahmed Sarkies <75149514+AhmedSarkies@users.noreply.github.com>
2023-02-27 15:49:58 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* _atoi - convert a string to an integer.
-* @s: the string to be comverted.
+*_atoi - convert a string to an integer.
+*@s: the string to be comverted.
 *Return: The integer value of the comverted string.
 **/
 int _atoi(char *s) 
