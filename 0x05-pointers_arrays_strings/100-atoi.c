49fc5ee3644d00a32bf83c997056b59582f9c7c4
Ahmed Sarkies <75149514+AhmedSarkies@users.noreply.github.com>
2023-02-27 15:33:17 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,26 +5,21 @@
  *
  *Return: The integer value of the comverted string.
  */
-int _atoi(char *s)
-{
+int _atoi(char *s) {
 int i = 0;
 int n = 0;
 int signo = 1;
-while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
-{
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0) {
 if (s[i] == '-')
 signo *= -1;
 i++;
 }
-while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
-{
-if (n >= 0)
-{
+while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0) {
+if (n >= 0) {
 n = n * 10 - (s[i] - '0');
 i++;
 }
-else
-{
+else {
 n = n * 10 - (s[i] - '0');
 i++;
 }
